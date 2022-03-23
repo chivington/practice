b7625e348ca9b4b46bb59ebc55ab38e9bc908c90
chivington <j.chivington@ieee.org>
2022-03-23 15:12:57 -0500

    Selection sort

--- sort/Selection.java
+++ sort/Selection.java
@@ -2,50 +2,90 @@
 class Selection {
 	// selection sort for integers
 	public void selectionSort(int[] arr) {
-		// pass over the array n-1 times
-		for (int j = 0; j < arr.length - 1; j++) {
-			int iMin = j;
-			for (int i = j+1; i < arr.length; i++) {
-				if (arr[i] < arr[iMin]) iMin = i;
+		// pass over the array n times
+		for (int i = 0; i < arr.length - 1; i++) {
+			// select current minimum
+			int jMin = i;
+			// pass through the remainder of the array
+			for (int j = i+1; j < arr.length; j++) {
+				// if the integer is smaller than the current minimum
+				// set the current minimum to the current integer
+				if (arr[j] < arr[jMin]) jMin = j;
 			}
-			if (iMin != j) {
-				int temp = arr[j];
-				arr[j] = arr[iMin];
-				arr[iMin] = temp;
+			// if the new current minimum isn't the current integer
+			// swap the current integer with the current minimum
+			if (jMin != i) {
+				int temp = arr[i];
+				arr[i] = arr[jMin];
+				arr[jMin] = temp;
 			}
 		}
 	}
 
 	// selection sort for string length
-	public void selectionSort(String[] arr) {
-		// pass over the array n-1 times
-		for (int j = 0; j < arr.length - 1; j++) {
-			int iMin = j;
-			for (int i = j+1; i < arr.length; i++) {
-				if (arr[i].length() < arr[iMin].length()) iMin = i;
+	public void selectionSort_len(String[] arr) {
+		// pass over the array n times
+		for (int i = 0; i < arr.length - 1; i++) {
+			// select current minimum
+			int jMin = i;
+			// pass through the remainder of the array
+			for (int j = i+1; j < arr.length; j++) {
+				// if the string is shorter than the current minimum
+				// set the current minimum to the current string
+				if (arr[j].length() < arr[jMin].length()) jMin = j;
 			}
-			if (iMin != j) {
-				String temp = arr[j];
-				arr[j] = arr[iMin];
-				arr[iMin] = temp;
+			// if the new current minimum isn't the current string
+			// swap the current string with the current minimum
+			if (jMin != i) {
+				String temp = arr[i];
+				arr[i] = arr[jMin];
+				arr[jMin] = temp;
+			}
+		}
+	}
+
+	// selection sort for alphabetical sorting; 1st letter only
+	public void selectionSort_abc(String[] arr) {
+		// pass over the array n times
+		for (int i = 0; i < arr.length - 1; i++) {
+			// select current minimum
+			int jMin = i;
+			// pass through the remainder of the array
+			for (int j = i+1; j < arr.length; j++) {
+				// if the 1st letter is smaller than the current minimum
+				// set the current minimum to the current string
+				if (arr[j].charAt(0) < arr[jMin].charAt(0)) jMin = j;
+			}
+			// if the new current minimum isn't the current string
+			// swap the current string with the current minimum
+			if (jMin != i) {
+				String temp = arr[i];
+				arr[i] = arr[jMin];
+				arr[jMin] = temp;
 			}
 		}
 	}
 
 	// test selectionSort algorithm
 	public static void main(String[] args) {
-		Selection b = new Selection();
+		Selection s = new Selection();
 
-		// create array of integers
+		// create array of integers & sort
 		int[] intArr = {3, 5, 2, 7, 8, 1, 4, 6, 9};
-		b.selectionSort(intArr);
+		s.selectionSort(intArr);
 		for (int i = 0; i < intArr.length; i++) System.out.println(" intArr["+i+"]: " + intArr[i]);
 		System.out.println("\n");
 
-		// create array of strings
-		String[] strArr = {"st", "string", "s", "stri", "strin", "str"};
-		b.selectionSort(strArr);
-		for (int i = 0; i < strArr.length; i++) System.out.println(" strArr["+i+"]: " + strArr[i]);
+		// create array of strings & sort
+		String[] strArr1 = {"st", "string", "s", "stri", "strin", "str"};
+		s.selectionSort_len(strArr1);
+		for (int i = 0; i < strArr1.length; i++) System.out.println(" strArr1["+i+"]: " + strArr1[i]);
+		System.out.println("\n");
+
+		// create array of strings & sort
+		String[] strArr2 = {"alligator", "car", "saturday", "bus", "fish"};
+		s.selectionSort_abc(strArr2);
+		for (int i = 0; i < strArr2.length; i++) System.out.println(" strArr2["+i+"]: " + strArr2[i]);
 		System.out.println("\n");
 	}
 }
